ab2b18bb51fd010c459e45546cd3bd19d4a8d8d5
DenysLobur <denismachtalles@gmail.com>
2025-08-10 04:18:04 +0300

    Animations - Ep05

--- app/src/main/java/com/example/game/GamePanel.java
+++ app/src/main/java/com/example/game/GamePanel.java
@@ -12,8 +12,8 @@ import android.view.SurfaceView;
 import androidx.annotation.NonNull;
 
 import com.example.game.entities.GameCharacters;
+import com.example.game.helpers.GameConstants;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
@@ -23,7 +23,14 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private float x, y;
     private Random rand = new Random();
     private GameLoop gameLoop;
-    private ArrayList<PointF> skeletons = new ArrayList<>();
+    //    private ArrayList<PointF> skeletons = new ArrayList<>();
+    private PointF skeletonPos;
+    private int skeletonDir = GameConstants.FaceDir.DOWN;
+    private long lastDirChange = System.currentTimeMillis();
+
+    private int playerAnimationIndexY, playerFaceDir = GameConstants.FaceDir.RIGHT;
+    private int animationTick;
+    private int animationSpeed = 10;
 
     public GamePanel(Context context) {
         super(context);
@@ -31,9 +38,11 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         holder.addCallback(this);
         paint.setColor(Color.BLUE);
 
-        for (int i = 0; i < 50; i++) {
-            skeletons.add(new PointF(rand.nextInt(1080), rand.nextInt(1920)));
-        }
+        skeletonPos = new PointF(rand.nextInt(1080), rand.nextInt(1920));
+
+//        for (int i = 0; i < 50; i++) {
+//            skeletons.add(new PointF(rand.nextInt(1080), rand.nextInt(1920)));
+//        }
 
         gameLoop = new GameLoop(this);
     }
@@ -42,22 +51,63 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
         Canvas c = holder.lockCanvas();
         c.drawColor(Color.BLACK);
 
-        c.drawBitmap(GameCharacters.PLAYER.getSpriteSheet(), 500, 500, null);
+        c.drawBitmap(GameCharacters.PLAYER.getSprite(playerAnimationIndexY, playerFaceDir), x, y, null);
 
-        c.drawBitmap(GameCharacters.PLAYER.getSprite(0, 0), x, y, null);
+        c.drawBitmap(GameCharacters.SKELETON.getSprite(playerAnimationIndexY, skeletonDir), skeletonPos.x, skeletonPos.y, null);
 
-        for (PointF skeleton : skeletons) {
-            c.drawBitmap(GameCharacters.SKELETON.getSprite(0, 0), skeleton.x, skeleton.y, null);
-        }
+//        for (PointF skeleton : skeletons) {
+//            c.drawBitmap(GameCharacters.SKELETON.getSprite(0, 0), skeleton.x, skeleton.y, null);
+//        }
+
         holder.unlockCanvasAndPost(c);
     }
 
     public void update(double delta) {
-        for (PointF skeleton : skeletons) {
-            skeleton.y += 300 * delta; // Move skeletons down at a speed of 300 pixels per second
-            if (skeleton.y > getHeight()) {
-                skeleton.y = 0; // Reset position if it goes off screen
-                skeleton.x = rand.nextInt(getWidth()); // Randomize x position
+        if (System.currentTimeMillis() - lastDirChange >= 3000) {
+            skeletonDir = rand.nextInt(4);
+            lastDirChange = System.currentTimeMillis();
+        }
+
+        switch (skeletonDir) {
+            case GameConstants.FaceDir.DOWN:
+                skeletonPos.y += delta * 300;
+                if (skeletonPos.y >= getHeight()) {
+                    skeletonDir = GameConstants.FaceDir.UP;
+                }
+                break;
+
+            case GameConstants.FaceDir.UP:
+                skeletonPos.y -= delta * 300;
+                if (skeletonPos.y <= 0) {
+                    skeletonDir = GameConstants.FaceDir.DOWN;
+                }
+                break;
+
+            case GameConstants.FaceDir.RIGHT:
+                skeletonPos.x += delta * 300;
+                if (skeletonPos.x >= getWidth()) {
+                    skeletonDir = GameConstants.FaceDir.LEFT;
+                }
+                break;
+
+            case GameConstants.FaceDir.LEFT:
+                skeletonPos.x -= delta * 300;
+                if (skeletonPos.x <= 0) {
+                    skeletonDir = GameConstants.FaceDir.RIGHT;
+                }
+                break;
+        }
+
+        updateAnimation();
+    }
+
+    private void updateAnimation() {
+        animationTick++;
+        if (animationTick >= animationSpeed) {
+            animationTick = 0;
+            playerAnimationIndexY++;
+            if (playerAnimationIndexY >= 4) {
+                playerAnimationIndexY = 0;
             }
         }
     }
@@ -65,8 +115,28 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            x = event.getX();
-            y = event.getY();
+            float newX = event.getX();
+            float newY = event.getY();
+
+            float xDiff = Math.abs(newX - x);
+            float yDiff = Math.abs(newY - y);
+
+            if (xDiff > yDiff) {
+                if (newX < x) {
+                    playerFaceDir = GameConstants.FaceDir.LEFT;
+                } else {
+                    playerFaceDir = GameConstants.FaceDir.RIGHT;
+                }
+            } else {
+                if (newY < y) {
+                    playerFaceDir = GameConstants.FaceDir.UP;
+                } else {
+                    playerFaceDir = GameConstants.FaceDir.DOWN;
+                }
+            }
+
+            x = newX;
+            y = newY;
         }
 
         return true;
